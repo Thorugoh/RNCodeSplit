7dfd34482a91b05842badcea07ded24c23fded9d
Victor Hugo <pavictorhugo@gmail.com>
2022-10-23 17:35:25 -0300

    create demo

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import { ScriptManager, Script } from "@callstack/repack/client";
+import App from './src/App';
+import {name as appName, localChunks} from './app.json';
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+    if (__DEV__) {
+        return {
+          url: Script.getRemoteURL("http://10.0.0.2:3000/${scriptId}"),
+          cache: false
+        };
+    }
+    if(localChunks.includes(scriptId)){
+        return { 
+            url: Script.getFileSystemURL(scriptId),
+        }
+    }
+})
+
+AppRegistry.registerComponent(appName, () => App);
